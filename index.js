21623c45a027aaba1a1c22456b6cb8a7c917ee24
mukitur <mukit.inway@gmail.com>
2023-10-15 00:42:57 +0600

    update done

--- index.js
+++ index.js
@@ -33,6 +33,34 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    // update
+    app.get('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+    app.put('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo,
+        },
+      };
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+    });
+
     // post coffee to DB
     app.post('/coffee', async (req, res) => {
       const newCoffee = req.body;
